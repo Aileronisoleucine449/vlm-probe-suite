c04173a18acbb47112c4bded08c60c60c425c898
Ka Yiu Cheung <257232627+mrvellang@users.noreply.github.com>
2023-07-08 21:00:33 +0800

    tiny tests for AttrSwap.summarize

---
+++ tests/test_probes.py
@@ -0,0 +1,34 @@
+import pytest
+from finevlm_probe.probes import get_probe
+from finevlm_probe.probes.attr_swap import AttrSwapProbe
+from finevlm_probe.probes.count_coarse import CountCoarseProbe
+
+
+def test_get_probe_known():
+    assert isinstance(get_probe("attr-swap"), AttrSwapProbe)
+    assert isinstance(get_probe("count-coarse"), CountCoarseProbe)
+
+
+def test_get_probe_unknown():
+    with pytest.raises(KeyError):
+        get_probe("not-a-thing")
+
+
+def test_attr_swap_summarize_empty():
+    p = AttrSwapProbe()
+    s = p.summarize([])
+    assert s["n"] == 0
+
+
+def test_attr_swap_summarize():
+    p = AttrSwapProbe()
+    recs = [
+        {"sample_id": "a", "correct": 1, "margin": 0.1},
+        {"sample_id": "b", "correct": 0, "margin": -0.2},
+        {"sample_id": "c", "correct": 1, "margin": 0.05},
+    ]
+    s = p.summarize(recs)
+    assert s["n"] == 3
+    assert abs(s["accuracy"] - 2/3) < 1e-6
+
+
